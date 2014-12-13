9ea12e29bee2bf59eed08757b84050faf2337066
Jesse Chan <jchan985@gmail.com>
2014-12-13 15:12:59 -0600

    comments

--- Init.m
+++ Init.m
@@ -1,8 +1,9 @@
+% initializes paths 
 
 pw = pwd;
 addpath([pw,'/Codes1D']);
 addpath([pw,'/Codes2D']);
 addpath([pw,'/Codes3D']);
 addpath([pw,'/pyramid']);
-addpath([pw,'/tet']);
+% addpath([pw,'/tet']);
 addpath(pw);
